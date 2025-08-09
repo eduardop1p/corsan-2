07744a6ab0c60401500259474064bf38d085d7cd
eduardop1p <el702985@gmail.com>
2025-08-09 08:05:31 -0300

    melhorias

--- src/app/api/create-transaction-pix/route.ts
+++ src/app/api/create-transaction-pix/route.ts
@@ -18,7 +18,6 @@ export async function POST(req: NextRequest) {
       shipping,
     } = body;
     amount = amount;
-    console.log(amount)
     items = items.map(item => ({ ...item, unitPrice: +item.unitPrice, quantity: +item.quantity })); // eslint-disable-line
 
     const newBody: TransactionPixProtocol = {
